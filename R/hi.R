aa76a8d9d7b3720ba59641377b0943bd7bd2716b
May_CA <May_CA>
2016-10-05 17:41:40 -0400

    implement how argument for function 'hi' - 'shout' for all caps - 'whisper' for lowercase

--- R/hi.R
+++ R/hi.R
@@ -6,24 +6,33 @@
 #' @param who character(1) The name of the person you wish to talk to start a
 #'   conversation with
 #'
+#' @param how char(1) how to greet the person.  Either "shout" (default) or
+#'   "whisper"
+#'
 #' @return character(1) A line of hello world with num of chars
 #'
 #' @examples
 #' hi("Paul")
-#'
+#' hi("Paul", "whisper")
 #'
 #' @export
 #'
-hi = function(who) {
+hi = function(who, how = c("shout", "whisper")) {
      #check value of who to be char, length 1, not NA, otherwise stop
      stopifnot(
           is.character(who),
           length(who) == 1,
           !is.na(who)
      )
-     paste("hello", shout(who),"you have", nchar(who), "letters in your name!")
+     how = match.arg(how)
+     fun = switch(how, shout = shout, whisper = whisper)
+     paste("hello", fun(who),"you have", nchar(who), "letters in your name!")
 }
 
 shout = function(who){
      toupper(who)
+}
+
+whisper = function(who){
+     tolower(who)
 }
